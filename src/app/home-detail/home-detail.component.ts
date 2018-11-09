9010df51dca78194150e4a73b4c13dcc956fa177
Colt Street <Colt.Street@dataventures.com>
2018-11-09 10:43:47 -0500

    detail styling

--- src/app/home-detail/home-detail.component.ts
+++ src/app/home-detail/home-detail.component.ts
@@ -1,6 +1,10 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ElementRef, ViewChild } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 import { DataService, IDataItem } from "../core/data.service";
+import { Page } from "tns-core-modules/ui/page/page";
+import { registerElement } from 'nativescript-angular/element-registry';
+import { CardView } from 'nativescript-cardview';
+registerElement('CardView', () => CardView);
 
 @Component({
     selector: "HomeDetail",
@@ -9,17 +13,25 @@ import { DataService, IDataItem } from "../core/data.service";
     styleUrls: ["./home-detail.component.scss"]
 })
 export class HomeDetailComponent implements OnInit {
+    @ViewChild('content') pageContent: ElementRef;
+    
     team: any;
     myImageSrc: any;
 
     constructor(
         private data: DataService,
-        private route: ActivatedRoute
+        private route: ActivatedRoute,
+        private page: Page
     ) { }
 
     ngOnInit(): void {
         const id = +this.route.snapshot.params.id;
         this.team = this.data.getHomeTeam();
         this.myImageSrc = "~/app/images/" + this.team.fields.team.stringValue + ".png";
+        let temp = this.page.getViewById("home");
+        temp.style.backgroundImage = this.myImageSrc;
+        temp.style.backgroundRepeat = 'no-repeat';
+        temp.style.backgroundPosition = 'center';
+        temp.style.backgroundSize = 'cover';
     }
 }
